c2e991e05cfe974e3bf1cd687ff636d7425dcb94
Yasir Shaikh <68939975+Yasir-Tec@users.noreply.github.com>
2023-11-02 19:21:17 -0700

    3/11

---
+++ Counting_Elements.cpp
@@ -0,0 +1,61 @@
+1,2,3 ---> 2
+1,1,2,3 -> 3
+
+
+
+Class Solutions
+{
+
+public:
+ 
+int countElements(vector<int>& arr>
+{
+
+
+unordered_map<int,int> _map;
+
+for(int a : arrr)
+{
+	if(_map.find(a) != _map.end())
+	{
+		_map[a]++;
+	}
+	else
+	{
+		_map[a]=1;
+	}
+	int result = 0;
+	for(auto  n:_map)
+	{
+		if(_map.find(n.first + 1) != _map.end())
+		{
+			results+=n.second;
+		}
+	}
+	returns results;
+}
+
+
+
+
+class Solution
+{
+    public int countElements(int[] arr) 
+    {
+        int count = 0;
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < arr.length; i++)
+        {
+            set.add(arr[i]);
+        }
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(set.contains(arr[i]+1))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
